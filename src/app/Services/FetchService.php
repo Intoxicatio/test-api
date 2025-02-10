b7d3c289ee8905f3d6435854f2670eb418011006
Intoxicatio <generalsunayaka@gmail.com>
2025-02-10 18:17:46 +0300

    fix urls and workers name

--- src/app/Services/FetchService.php
+++ src/app/Services/FetchService.php
@@ -14,9 +14,9 @@ class FetchService
         $today = Carbon::now()->format('Y-m-d');
         if ($type = 'stocks') {
             $tommorow = Carbon::now()->addDay()->format('Y-m-d');
-            $url = "89.108.115.241:6969/api/{$type}?dateFrom={$today}&dateTo={$tommorow}&page=1&limit=500&key=E6kUTYrYwZq2tN4QEtyzsbEBk3ie";
+            $url = "89.108.115.241:6969/api/{$type}?dateFrom={$today}&dateTo={$tommorow}&limit=500&key=E6kUTYrYwZq2tN4QEtyzsbEBk3ie";
         } else {
-            $url = "89.108.115.241:6969/api/{$type}?dateFrom=0001-01-01&dateTo={$today}&page=1&limit=500&key=E6kUTYrYwZq2tN4QEtyzsbEBk3ie";
+            $url = "89.108.115.241:6969/api/{$type}?dateFrom=0001-01-01&dateTo={$today}&limit=500&key=E6kUTYrYwZq2tN4QEtyzsbEBk3ie";
         }
         $maxPage  = Http::get($url)->json()['meta']['last_page'];
         while ($maxPage > $page) {
