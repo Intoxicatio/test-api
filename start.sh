72562b1027097d1bb0c8175db64e78a17ad5b2b3
Intoxicatio <generalsunayaka@gmail.com>
2025-02-17 00:36:31 +0300

    server

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
-echo "Запуск supervisord..."
-exec supervisord -c /etc/supervisord.conf
+php artisan octane:start  --server=swoole --host=0.0.0.0 --port=8000
+# echo "Запуск supervisord..."
+# exec supervisord -c /etc/supervisord.conf
